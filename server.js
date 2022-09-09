7b24321467ece0a8fbc7e4e1fac4c1bb3ef6e34b
kanngji <50470748+kanngji@users.noreply.github.com>
2022-09-09 14:44:45 +0900

    include header , footer by ejs

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
+const bodyparser = require("body-parser");
+const path = require("path");
 
 const app = express();
 
@@ -11,8 +13,21 @@ const PORT = process.env.PORT || 8080;
 // log request
 app.use(morgan("tiny"));
 
+// parse request to body-parser
+app.use(bodyparser.urlencoded({ extended: true }));
+
+// set view engine
+app.set("view engine", "ejs");
+// app.set("views",path.resolve(__dirname,"views/ejs"));
+
+//load assets
+app.use("/css", express.static(path.resolve(__dirname, "assets/css")));
+app.use("/img", express.static(path.resolve(__dirname, "assets/img")));
+app.use("/js", express.static(path.resolve(__dirname, "assets/js")));
+
+// css/style.css
 app.get("/", (req, res) => {
-  res.send("CRUD Application");
+  res.render("index");
 });
 
 app.listen(PORT, () => {
